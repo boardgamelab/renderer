c70e4bb5d10d2c501a006142ae937b2ef07d7f11
Nicolo Davis <nicolodavis@gmail.com>
2020-04-16 22:19:47 +0800

    nicer drag animations

--- src/objects/GameObject.svelte
+++ src/objects/GameObject.svelte
@@ -4,6 +4,9 @@
   import Card from './card/Card.svelte';
   import CardHolder from './card-holder/CardHolder.svelte';
   import { MergeOpts } from '../merge.ts';
+  import { writable, derived } from 'svelte/store';
+  import { tweened } from 'svelte/motion';
+  import { expoOut } from 'svelte/easing';
 
   export let id;
 
@@ -13,31 +16,62 @@
   const { templateID } = schema.objects[id];
   const template = schema.templates[templateID];
 
-  let x;
-  let y;
+  const sizeOffset = tweened(
+    { dx: 0, dy: 0 },
+    {
+      duration: 300,
+      easing: expoOut,
+    }
+  );
+  const rawPosition = writable({ x: 0, y: 0 });
+  const positionOffset = tweened(
+    { dx: 0, dy: 0 },
+    {
+      duration: 300,
+      easing: expoOut,
+    }
+  );
+  const position = derived(
+    [rawPosition, positionOffset],
+    ([$rawPosition, $positionOffset]) => {
+      return {
+        x: $rawPosition.x + $positionOffset.dx,
+        y: $rawPosition.y + $positionOffset.dy,
+      };
+    }
+  );
+
   $: {
     const opts = MergeOpts(schema, $state, id);
-    x = opts.x || 0;
-    y = opts.y || 0;
+    const x = opts.x || 0;
+    const y = opts.y || 0;
+    rawPosition.set({ x, y });
   }
 
   const { drag, mouseup, activeObjectID } = getContext('mouse');
 
   let dragStart = null;
   function MouseDown(e) {
-    dragStart = { x, y };
+    // Ignore right-clicks.
+    if (e.button !== 0) {
+      return;
+    }
+    dragStart = { x: $rawPosition.x, y: $rawPosition.y };
+    positionOffset.set({ dx: -50, dy: -50 });
     activeObjectID.set(id);
   }
 
   drag.subscribe(e => {
     if (dragStart === null) return;
-    x = dragStart.x + e.delta.x;
-    y = dragStart.y + e.delta.y;
+    const x = dragStart.x + e.delta.x;
+    const y = dragStart.y + e.delta.y;
+    rawPosition.set({ x, y });
   });
 
   mouseup.subscribe(() => {
     if (dragStart) {
       dragStart = null;
+      positionOffset.set({ dx: 0, dy: 0 });
 
       state.update(s => ({
         ...s,
@@ -47,8 +81,8 @@
             ...s.objects[id],
             opts: {
               ...s.objects[id].opts,
-              x,
-              y,
+              x: $rawPosition.x,
+              y: $rawPosition.y,
             },
           },
         },
@@ -59,12 +93,12 @@
 </script>
 
 <g {id} on:mousedown={MouseDown}>
-  <title>{id}</title>
   <svelte:component
     this={component}
     objectID={id}
     {schema}
     {template}
-    {x}
-    {y} />
+    {sizeOffset}
+    {position}
+    isDragging={dragStart !== null} />
 </g>
--- src/objects/card/Card.svelte
+++ src/objects/card/Card.svelte
@@ -1,18 +1,30 @@
 <script>
-  export let x;
-  export let y;
+  export let position;
   export let schema;
   export let objectID;
   export let template;
+  export let isDragging = false;
+  export let sizeOffset;
 
   import { getContext } from 'svelte';
-
   const renderer = getContext('renderer');
 
-  const { width, height } = template.geometry;
+  let { x, y } = $position;
+  let { width, height } = template.geometry;
   const fill = '#fff';
   const stroke = '#111';
+
+  $: {
+    x = $position.x;
+    y = $position.y;
+    width = template.geometry.width + $sizeOffset.dx;
+    height = template.geometry.height + $sizeOffset.dy;
+  }
 </script>
+
+{#if isDragging}
+  <rect x={x + 30} y={y + 30} {width} {height} fill="#555" fill-opacity="5%" />
+{/if}
 
 <rect {x} {y} {width} {height} {fill} {stroke} />
 
