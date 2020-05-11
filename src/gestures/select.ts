55bd3c13992bfa863d0994e8e836ba9b59344d9a
Nicolo Davis <nicolodavis@gmail.com>
2020-05-11 20:35:26 +0800

    also dispatch custom event during selection

--- src/gestures/select.ts
+++ src/gestures/select.ts
@@ -30,6 +30,7 @@ export function select(node: Element, opts: Opts) {
     const target = (e.target as Element).closest('[data-selectable=true]');
 
     if (target) {
+      target.dispatchEvent(new CustomEvent('select'));
       const id = (target as HTMLElement).dataset.id as string;
       o.activeObjects.update(() => ({ [id]: true }));
     }
