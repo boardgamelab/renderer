20cfe175a776c960d7b92fbcf7a7e18bba1d60a7
Nicolo Davis <nicolodavis@gmail.com>
2020-06-15 15:52:12 +0800

    add geometry to card holder

--- src/objects/card-holder/CardHolder.svelte
+++ src/objects/card-holder/CardHolder.svelte
@@ -1,9 +1,24 @@
 <script>
+  export let id;
   export let x;
   export let y;
-  export let width = 150;
-  export let height = 200;
   export let color = '#eee';
+
+  import { getContext } from 'svelte';
+  const schema = getContext('schema');
+
+  let geometry = { width: 0, height: 0 };
+
+  let { width, height } = geometry;
+
+  $: {
+    if (id in $schema.objects) {
+      const { templateID } = $schema.objects[id];
+      const template = $schema.templates[templateID];
+      width = template.geometry.width;
+      height = template.geometry.height;
+    }
+  }
 </script>
 
 <rect {x} {y} {width} {height} fill={color} />
