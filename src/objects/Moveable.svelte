91120912e17e765c844760070f5a43dbef260293
Nicolo Davis <nicolodavis@gmail.com>
2020-06-19 19:52:29 +0800

    capture more animations from remote moves

--- src/objects/Moveable.svelte
+++ src/objects/Moveable.svelte
@@ -23,7 +23,28 @@
     if (id in $state.objects) {
       x = $state.objects[id].x || 0;
       y = $state.objects[id].y || 0;
-      if ($state.remote) {
+
+      if ($state.remote && $state.previousPositions) {
+        Object.entries($state.previousPositions).forEach(([key, value]) => {
+          if (key !== id) return;
+
+          let px = value.x || 0;
+          let py = value.y || 0;
+
+          if (value.type === 'in-container') {
+            if (value.x !== undefined || value.y !== undefined) {
+              px = value.x || 0;
+              py = value.y || 0;
+            } else if (value.id in $state.objects) {
+              const { x, y } = $state.objects[value.id];
+              px = x || 0;
+              py = y || 0;
+            }
+          }
+
+          position.set(RelativeToParent({ x: px, y: py }));
+        });
+
         position.set({ x, y }, { duration: 100 });
       } else {
         position.set({ x, y });
