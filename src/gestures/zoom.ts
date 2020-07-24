fecbb3e8b20ecfc2b0f60e90f9d53f14e9b58c18
Nicolo Davis <nicolodavis@gmail.com>
2020-07-24 13:50:33 +0800

    allow zooming out more

--- src/gestures/zoom.ts
+++ src/gestures/zoom.ts
@@ -20,7 +20,7 @@ interface Opts {
   zoomLevel: Writable<number>;
 }
 
-const MAX_ZOOM = 7;
+const MAX_ZOOM = 20;
 const MIN_ZOOM = 2;
 const ZOOM_IN_MULTIPLIER = 1.3;
 const ZOOM_OUT_MULTIPLIER = 0.7;
@@ -31,9 +31,9 @@ const ZOOM_OUT_MULTIPLIER = 0.7;
 export function zoom(_: Element, opts: Opts) {
   function MouseWheel(e: WheelEvent) {
     if (e.deltaY > 0) {
-      opts.zoomLevel.update(v => Math.min(v * ZOOM_IN_MULTIPLIER, MAX_ZOOM));
+      opts.zoomLevel.update((v) => Math.min(v * ZOOM_IN_MULTIPLIER, MAX_ZOOM));
     } else {
-      opts.zoomLevel.update(v => Math.max(v * ZOOM_OUT_MULTIPLIER, MIN_ZOOM));
+      opts.zoomLevel.update((v) => Math.max(v * ZOOM_OUT_MULTIPLIER, MIN_ZOOM));
     }
   }
 
