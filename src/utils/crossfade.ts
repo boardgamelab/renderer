eca126a00ed727756d85809c5c40d7549239409e
Nicolo John Davis <nicolodavis@gmail.com>
2021-06-05 16:36:58 +0800

    refactor crossfade.ts

--- src/utils/crossfade.ts
+++ src/utils/crossfade.ts
@@ -8,6 +8,7 @@ interface Point {
 interface Params {
   key: string;
   toSVGPoint: (point: Point) => Point;
+  parentID: string;
   ghost?: boolean;
   disable?: boolean;
   hand?: boolean;
@@ -21,113 +22,103 @@ type NodeInfo = Params & {
   rect: DOMRect;
 };
 
-export function crossfade() {
-  const receivers = new Map();
-  const senders = new Map();
+export const senders = new Map();
 
-  function crossfade(from: NodeInfo, node: Element, params: Params) {
-    const { delay = 0, duration = 250, easing = cubicOut } = params;
+function crossfade(from: NodeInfo, node: Element, params: Params) {
+  const { delay = 0, duration = 250, easing = cubicOut } = params;
 
-    const rect = node.getBoundingClientRect();
+  const rect = node.getBoundingClientRect();
 
-    let a = { x: rect.left, y: rect.top };
-    let b = { x: from.rect.left, y: from.rect.top };
+  let a = { x: rect.left, y: rect.top };
+  let b = { x: from.rect.left, y: from.rect.top };
 
-    if (!params.hand && !params.ghost) {
-      a = params.toSVGPoint(a);
-      b = params.toSVGPoint(b);
-    }
-
-    const dx = b.x - a.x;
-    const dy = b.y - a.y;
-    const dw = from.rect.width / rect.width;
-    const dh = from.rect.height / rect.height;
-
-    let transform: string | null = null;
-
-    if (params.hand || params.ghost) {
-      const style = getComputedStyle(node);
-      if (style.transform !== 'none') {
-        transform = style.transform;
-      }
-    } else {
-      transform = node.getAttribute('transform');
-    }
-
-    return {
-      delay,
-      duration,
-      easing,
-      tick: (t: number, u: number) => {
-        const ux = u * dx;
-        const uy = u * dy;
-        let uw = t + u * dw;
-        let uh = t + u * dh;
-
-        if (isNaN(uw)) uw = 1;
-        if (isNaN(uh)) uh = 1;
-
-        if (params.ghost || params.hand) {
-          let value = `translate3d(${ux}px, ${uy}px, 0) scale(${uw}, ${uh})`;
-          value = transform ? `${transform} ${value}` : value;
-          (node as HTMLElement).style.transform = value;
-        } else {
-          let value = `translate(${ux} ${uy}) scale(${uw} ${uh})`;
-          value = transform ? `${transform} ${value}` : value;
-          node.setAttribute('transform', value);
-        }
-      },
-    };
+  if (!params.hand && !params.ghost) {
+    a = params.toSVGPoint(a);
+    b = params.toSVGPoint(b);
   }
 
-  function transition(items: typeof senders, counterparts: typeof receivers) {
-    return (node: Element, params: Params) => {
-      if (params.disable) {
-        return { duration: 0 };
-      }
+  const dx = b.x - a.x;
+  const dy = b.y - a.y;
+  const dw = from.rect.width / rect.width;
+  const dh = from.rect.height / rect.height;
 
-      items.set(params.key, {
+  let transform: string | null = null;
+
+  if (params.hand || params.ghost) {
+    const style = getComputedStyle(node);
+    if (style.transform !== 'none') {
+      transform = style.transform;
+    }
+  } else {
+    transform = node.getAttribute('transform');
+  }
+
+  return {
+    delay,
+    duration,
+    easing,
+    tick: (t: number, u: number) => {
+      const ux = u * dx;
+      const uy = u * dy;
+      let uw = t + u * dw;
+      let uh = t + u * dh;
+
+      if (isNaN(uw)) uw = 1;
+      if (isNaN(uh)) uh = 1;
+
+      if (params.ghost || params.hand) {
+        let value = `translate3d(${ux}px, ${uy}px, 0) scale(${uw}, ${uh})`;
+        value = transform ? `${transform} ${value}` : value;
+        (node as HTMLElement).style.transform = value;
+      } else {
+        let value = `translate(${ux} ${uy}) scale(${uw} ${uh})`;
+        value = transform ? `${transform} ${value}` : value;
+        node.setAttribute('transform', value);
+      }
+    },
+  };
+}
+
+export function send(node: Element, params: Params) {
+  if (!params.disable) {
+    const value = senders.get(params.key);
+
+    if (!value || !value.ghost) {
+      senders.set(params.key, {
         rect: node.getBoundingClientRect(),
         ...params,
       });
+    }
 
-      return () => {
-        if (params.animate === false) {
-          return { duration: 0 };
-        }
-
-        if (counterparts.has(params.key)) {
-          const from = counterparts.get(params.key);
-          counterparts.delete(params.key);
-          items.delete(params.key);
-          return crossfade(from, node, params);
-        }
-
-        // If the item just deleted is the ghost, then keep it around
-        // for a little while so that if the object subsequently
-        // reappears, it can animate itself from the ghost's position.
-        // The reason that this hack is necessary is because the ghost
-        // disappears in one microtask before the actual element is
-        // killed and transferred to its new location.
-        // TODO: Explore an alternative where the ghost can be killed
-        // in the same microtask as the element that it substitutes.
-        setTimeout(
-          () => {
-            items.delete(params.key);
-          },
-          params.ghost ? 500 : 0
-        );
-
-        return {
-          duration: 0,
-        };
-      };
-    };
+    setTimeout(() => {
+      senders.delete(params.key);
+    }, 50);
   }
 
-  return [transition(senders, receivers), transition(receivers, senders)];
+  return { duration: 0 };
 }
 
-const [send, receive] = crossfade();
+export function receive(node: Element, params: Params) {
+  return () => {
+    let animation = {
+      duration: 0,
+    };
 
-export { send, receive };
+    if (senders.has(params.key)) {
+      let other = senders.get(params.key);
+
+      if (senders.has(other.parentID)) {
+        other = senders.get(other.parentID);
+      }
+
+      // If the object has the same parent before and after,
+      // then it's likely that the container was moved (and
+      // we needn't transition the children).
+      if (params.parentID !== other.parentID) {
+        animation = crossfade(other, node, params);
+      }
+    }
+
+    return animation;
+  };
+}
