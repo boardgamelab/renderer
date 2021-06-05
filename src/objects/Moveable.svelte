8799d60ceadf351d211197b9c9e42cfe17f03e09
Nicolo John Davis <nicolodavis@gmail.com>
2021-06-05 17:12:08 +0800

    update Moveable.svelte

--- src/objects/Moveable.svelte
+++ src/objects/Moveable.svelte
@@ -1,7 +1,7 @@
 <script>
   import { createEventDispatcher, getContext } from 'svelte';
-  import { Drop } from './moveable.ts';
   import { tweened } from 'svelte/motion';
+  import { Drop } from './moveable.ts';
   import { send, receive } from '../utils/crossfade.ts';
   import { ghost } from '../ghost/ghost.ts';
 
@@ -15,31 +15,30 @@
 
   const highlight = getContext('highlight');
   const toSVGPoint = getContext('to-svg-point');
-  const ghostAPI = getContext('ghost');
   const viewOnly = getContext('viewOnly');
-
-  const position = tweened(null, { duration: 0 });
 
   $: active = id in $activeObjects;
 
-  let x;
-  let y;
-
-  $: {
-    x = obj.stateVal.x || 0;
-    y = obj.stateVal.y || 0;
-    position.set({ x, y }, { duration: 150 });
-  }
-
-  let isDragging = false;
+  let hide = false;
   let dragged = false;
-  let snapshot = null;
 
   function DragStart() {
     dispatch("movestart");
     dragged = false;
-    isDragging = true;
-    snapshot = $position;
+    hide = true;
+  }
+
+  let x = obj.stateVal.x || 0;
+  let y = obj.stateVal.y || 0;
+
+  const position = tweened({ x, y }, { duration: 0 });
+
+  $: {
+    if (obj.stateVal.x !== x || obj.stateVal.y !== y) {
+      x = obj.stateVal.x;
+      y = obj.stateVal.y;
+      position.set({ x, y }, { duration: 150 });
+    }
   }
 
   async function DragEnd({ detail }) {
@@ -47,7 +46,15 @@
 
     highlight.set({});
 
-    isDragging = false;
+    // If the object switches containers, we don't
+    // have to restore visibility because the object will be created
+    // anew at then new destination. If we did restore visibility in
+    // those cases, there will be a slight flickering artifact as the
+    // object becomes visible right before it disappears again as it
+    // is destroyed.
+    if (detail.dropID === parentID) {
+      hide = false;
+    }
 
     if (!dragged) {
       return;
@@ -73,20 +80,6 @@
           });
         }
       }
-
-      // If we're dropping the object back on its existing parent,
-      // undo any position changes.
-      if (detail.dropID === parentID) {
-        position.set(
-          {
-            x: obj.stateVal.x || 0,
-            y: obj.stateVal.y || 0,
-          },
-          { duration: 150 }
-        );
-
-        return;
-      }
     }
 
     const absolutePosition = { x: detail.svg.targetX, y: detail.svg.targetY };
@@ -107,11 +100,6 @@
     activeObjects.set({
       [id]: true,
     });
-
-    position.set({
-      x: snapshot.x + detail.svg.dx,
-      y: snapshot.y + detail.svg.dy,
-    });
   };
 </script>
 
@@ -121,17 +109,20 @@
   }
 </style>
 
+<!-- The object has to continue existing while the ghost is
+     active so that the ghost is not destroyed. Instead, we hide
+     it using CSS -->
 <g
-  use:ghost={{ api: $ghostAPI, disable: $viewOnly, onTable: true, parentID }}
-  transform="translate({$position.x}, {$position.y})"
-  class:hide={isDragging}
-  out:send={{ key: id, toSVGPoint, disable: isDragging }}
-  in:receive={{ key: id, toSVGPoint }}
+  use:ghost={{ id, disable: $viewOnly, onTable: true, parentID }}
+  transform="translate({$position.x || 0}, {$position.y || 0})"
+  class:hide
+  out:send={{ key: id, toSVGPoint, parentID }}
+  in:receive={{ key: id, toSVGPoint, parentID }}
   data-id={id}
   data-component={obj.stateVal.componentID}
   data-draggable={draggable}
   on:movestart={!$viewOnly && DragStart}
   on:moveend={!$viewOnly && DragEnd}
   on:move={!$viewOnly && Drag}>
-  <slot {active} {isDragging} />
+  <slot {active} {hide} />
 </g>
